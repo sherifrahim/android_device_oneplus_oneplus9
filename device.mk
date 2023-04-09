0d1f9dcd46985e47c85bb743dd87e0080805ef27
Sherif Rahim <28657313+sherifrahim@users.noreply.github.com>
2023-04-09 13:19:22 +0530

    oos muppets cam

--- device.mk
+++ device.mk
@@ -15,6 +15,9 @@
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+$(call inherit-product-if-exists, vendor/oplus/camera/oplus-camera.mk)
+
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
